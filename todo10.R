77d2dc3c711c9e3e327b16a8161683fe35af2fd4
axu10 <axu10@myseneca.ca>
2017-02-02 23:03:04 -0500

    Delete todo10.R

--- todo10.R
+++
@@ -1,7 +0,0 @@
-d = read.table(file = "/home/axu10/Desktop/SRT411/tst1.txt", header = TRUE)
-d1 = data.frame(a = c(1,2,4,8,16,32),
-                g = c(2,4,8,16,32,64),
-                x = c(3,6,12,24,48,96))
-d1
-write.table(d1, file = "/home/axu10/Desktop/SRT411/tst2.txt", 
-            row.names = FALSE)
